c30fb45d9dfd12952953f6a7fdcb25ecfe32fede
Sarah Ferreira <sarahferreira252@gmail.com>
2019-06-26 17:12:15 -0300

    finalizando php cadastro

--- Projeto/cadastro.php
+++ Projeto/cadastro.php
@@ -4,18 +4,55 @@ server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "studiodebeleza");
  
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if($link === false)
+{
+    die("ERRO: Não foi possível conectar" . mysqli_connect_error());
 }
  
+$login = $_POST['tEmail'];
+$senha = $_POST['tSenha'];
 // Attempt insert query execution
-$sql = "INSERT INTO usuario (email, senha) VALUES ($_POST["$tEmail"],$_POST["$tSenha"]);
-if(mysqli_query($link, $sql)){
-    echo "Records inserted successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+$sql = "INSERT INTO usuario (email, senha) VALUES ($login, $senha)";
+    if(mysqli_query($link, $sql))
+    {
+        echo "Cadastro Concluído com Sucesso :)";
+    } else
+        {
+            echo "ERRO: Não foi possível executar o $sql. " . mysqli_error($link);
+        }
+
+$id_usuario = SELECT id FROM usuario WHERE email = $tEmail //$id_usuario = SELECT id FROM usuario WHERE email=$temail
+$nome = $_POST["tNome"];
+$tipoUsuario = $_POST["tTipoUsuario"];
+$tel = $_POST["$tTelefone"];
+$calendario = $_POST["$tCalendario"];
+$cpf = $_POST["tCpf"];
+$sexo = $_POST["$tSexo"];
+if ($tipoUsuario == 2) // if tipo == funcionario
+{
+    $id_func = $id_usuario // id_func = $id_usuario
+    $sql = "INSERT INTO Funcionario (id_func, nome_funcionario, telefone, data_nasc, cpf, sexo) VALUES ($id_func, $nome, $tel, $calendario, $cpf, $sexo)";
+    if(mysqli_query($link, $sql))
+    {
+        echo "Cadastro concluído com sucesso! :)";
+    } else
+        {
+            echo "ERRO: Não foi possível executar o $sql. " . mysqli_error($link);
+        }
+
 }
- 
-// Close connection
+if ($tTipoUsuario == 1) // if tipo == cliente
+{
+    id_cliente = $id_usuario // id_cliente = $id_usuario
+    $sql = "INSERT INTO Cliente (id_cliente, nome_cliente, telefone, data_nasc, cpf, sexo) VALUES ($id_cliente, $nome, $tel, $calendario, $cpf, $sexo)";
+        if(mysqli_query($link, $sql))
+        {
+            echo "Cadastro concluído com sucesso! :)";
+        } else
+            {
+                echo "ERRO: Não foi possível executar o $sql. " . mysqli_error($link);
+            }
+}
+// FECHAR CONEXÃO
 mysqli_close($link);
 ?>
