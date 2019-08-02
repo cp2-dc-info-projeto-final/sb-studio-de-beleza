4e4da5582ed56a663484d10c729abf4af555b1e6
Leticia Lopes <lelopes.correa26@gmail.com>
2019-08-02 10:23:27 -0300

    Resolvendo exclusão de dados

--- Projeto/painel_adm/excluir.php
+++ Projeto/painel_adm/excluir.php
@@ -6,9 +6,14 @@ $connection = mysqli_connect("localhost", "root", "", "studiodebeleza");
 if(!$connection) die("Falha ao conectar ao banco");
 mysqli_select_db($connection, "studiodebeleza");
 
-$sql = "DELETE FROM funcionario WHERE id_func = '$id' ";
+$sql = "DELETE FROM usuario WHERE id_usuario = '$id' ";
 
 $result = mysqli_query($connection, $sql) or die ("Houve um erro");
- echo '<script> location.href="lista.php"; </script>';
+
+ $sql = "DELETE FROM funcionario WHERE id_func = '$id' ";
+
+ $result = mysqli_query($connection, $sql) or die ("Houve um erro");
+  
+ echo '<script> location.href="lista_func.php"; </script>';
 
 ?>
