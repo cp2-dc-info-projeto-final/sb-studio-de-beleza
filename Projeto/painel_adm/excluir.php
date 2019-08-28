2298c5df4a5d3acba35ae134639555fe3d8f1c61
EmanuelledeAraujo <lelopes.correa26@gmail.com>
2019-08-28 16:23:37 -0300

    correções

--- Projeto/painel_adm/excluir.php
+++ Projeto/painel_adm/excluir.php
@@ -2,15 +2,19 @@
 <?php
 require ("lista_funcView.php");
 
+$id_func = $_GET['id'];
+$id_usuario = $_GET['id'];
+
 $connection = mysqli_connect("localhost", "root", "", "studiodebeleza"); 
 if(!$connection) die("Falha ao conectar ao banco");
 mysqli_select_db($connection, "studiodebeleza");
 
-$sql = "DELETE FROM usuario WHERE id_usuario = '$id' ";
+$sql = "DELETE FROM usuario WHERE usuario.id_usuario = '$id_usuario'";
 
 $result = mysqli_query($connection, $sql) or die ("Houve um erro");
+ 
 
- $sql = "DELETE FROM funcionario WHERE id_func = '$id' ";
+$sql = "DELETE FROM Funcionario WHERE funcionario.id_func = '$id_func'";
 
  $result = mysqli_query($connection, $sql) or die ("Houve um erro");
   
