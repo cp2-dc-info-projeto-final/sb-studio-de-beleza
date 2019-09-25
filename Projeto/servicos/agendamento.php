c12b6278eda900f9f9f27ab59c6672a3fd6b170c
labcaxias <labcaxias@CDCLAB_05.labdc.zentyal>
2019-09-25 16:48:08 -0300

    adicionando botões pag serviços

---
+++ Projeto/servicos/agendamento.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="UTF-8">
+     <link rel="stylesheet" href="../jquery_css/bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="paginainicial.css">
+    <div class="logo_studiodiv">
+    <img id="logo_studio" src="../fotos_icones/logo_editada.png" width="120">  <!-- provavelmente editar a logo, pois ainda não há o vetor da mesma -->
+    </div>
+</head> 
+
+
+
+
+
+
+</div><footer class="footer">
+                <div class="row">
+                  <div class="col-6">
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> Tel:. (00) 0000-0000</p>
+                        <p><i class="fa fa-envelope" aria-hidden="true"></i> evelyn@evelyn.com</p>
+                  </div>
+                  <div class="col-6" style="text-align: right;">
+                    <h3>Studio de Beleza Evelyn Marins</h3>
+                    <p>Rua José Ferreira Murro, Nº 320 - Nova Iguaçu <br>
+                      Rio de Janeiro - RJ - 26031-170
+                      </p>
+                    </ul>
+                 
+                  </div>
+                </div>
+                <hr></hr>
+                <div class="row">
+                  <div class="col-12"><a href="https://pt-br.facebook.com/studioevelynmarins/"><img src="facebook-png-cinza-2.png" title="Facebook"  class="redessociais"/></a>
+                </div>
+            </footer>
