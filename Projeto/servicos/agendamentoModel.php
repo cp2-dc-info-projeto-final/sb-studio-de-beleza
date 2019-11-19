1be53adf6ae93ccd66ad6d7e2cdfcb4e02f12326
labcaxias <labcaxias@CDCLAB_09.labdc.zentyal>
2019-11-19 10:49:32 -0300

    add

--- Projeto/servicos/agendamentoModel.php
+++ Projeto/servicos/agendamentoModel.php
@@ -17,15 +17,14 @@ session_start();
 function cadastraAgendamento($id_servico, $id_func, $data_agend, $horario) {
   $sql = "INSERT INTO Agendamento (id_servico, id_func, data_agend, hora_inicio) VALUES ('$id_servico', '$id_func', '$data_agend', '$horario')"; 
   $result = mysqli_query($link, $sql);
+
 }
 //----------------------------------------------------------------------------------------
 
 $tempoocupado = "SELECT hora_inicio FROM Agendamento WHERE id_func = '.$id_func.' AND data_agend = '.$data_agend.'";
 $result = mysqli_query($link, $tempoocupado);
 
-while ($row = mysql_fetch_row($result)) {
-  $row['hora_inicio'];
-}
+
 
 
 $hora = array ('9:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00');
