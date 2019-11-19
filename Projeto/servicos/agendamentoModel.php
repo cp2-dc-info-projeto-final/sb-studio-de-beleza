e2318129015a048b512d3704cd136fafc71cf704
Emerson Marcelo <mceloemerson@gmail.com>
2019-11-19 10:43:39 -0300

    mechendo no agendamento

--- Projeto/servicos/agendamentoModel.php
+++ Projeto/servicos/agendamentoModel.php
@@ -14,18 +14,14 @@ if($link === false)
 
 session_start();
 
-function cadastraAgendamento($id_servico, $id_func, $data_agend, $horario) {
+function cadastraAgendamento($id_servico, $id_func, $data_agend, $horario, $selecione, $selecione1, $nulo, $nulofunc) {
   $sql = "INSERT INTO Agendamento (id_servico, id_func, data_agend, hora_inicio) VALUES ('$id_servico', '$id_func', '$data_agend', '$horario')"; 
   $result = mysqli_query($link, $sql);
-}
+
 //----------------------------------------------------------------------------------------
 
 $tempoocupado = "SELECT hora_inicio FROM Agendamento WHERE id_func = '.$id_func.' AND data_agend = '.$data_agend.'";
 $result = mysqli_query($link, $tempoocupado);
-
-while ($row = mysql_fetch_row($result)) {
-  $row['hora_inicio'];
-}
 
 
 $hora = array ('9:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00');
@@ -40,10 +36,17 @@ for ($i; 0 <= 7 ; $i--){
   Break;
 }
 
+// ---------------------------------------------------------------------------------------
+
+if ($selecione != $nulo && $selecione1 != $nulofunc){
+  function deixarOculto(){
+    document.getElementById('$data_agend').style.display = "none";
+   }
+}
+
+}
 
 
-
-//------------------------------------------------------------------------------------------
 
 
 
