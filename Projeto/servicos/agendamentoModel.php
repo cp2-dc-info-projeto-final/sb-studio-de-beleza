007cf482e261660d77a4c9df2f622df1f1b635a3
SarahFerreira <sarahferreira252@gmail.com>
2019-11-20 07:57:41 -0300

    adicionando arquivos atualizados

--- Projeto/servicos/agendamentoModel.php
+++ Projeto/servicos/agendamentoModel.php
@@ -17,17 +17,14 @@ session_start();
 function cadastraAgendamento($id_servico, $id_func, $data_agend, $horario) {
   $sql = "INSERT INTO Agendamento (id_servico, id_func, data_agend, hora_inicio) VALUES ('$id_servico', '$id_func', '$data_agend', '$horario')"; 
   $result = mysqli_query($link, $sql);
-}
-//----------------------------------------------------------------------------------------
+
 
 $tempoocupado = "SELECT hora_inicio FROM Agendamento WHERE id_func = '.$id_func.' AND data_agend = '.$data_agend.'";
 $result = mysqli_query($link, $tempoocupado);
 
 while ($row = mysql_fetch_row($result)) {
   $row['hora_inicio'];
-}
-
-
+  
 $hora = array ('9:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00');
 $i = 0;
 
@@ -39,11 +36,10 @@ for ($i; 0 <= 7 ; $i--){
   }
   Break;
 }
+}
 
+}
 
-
-
-//------------------------------------------------------------------------------------------
 
 
 
