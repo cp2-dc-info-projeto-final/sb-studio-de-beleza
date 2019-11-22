7f0c29e54433923188cc2d046583bf04726bf9dd
Emerson Marcelo <mceloemerson@gmail.com>
2019-11-22 14:56:25 -0300

    ajustando padrão utf8

--- src/connection_factory.php
+++ src/connection_factory.php
@@ -2,7 +2,7 @@
 
     function get_connection() {
         $conn = mysqli_connect("localhost", "root", "", "studiodebeleza"); /* Iniciando a conexão com o banco de dados */
-        mysqli_set_charset($conn,'utf-8');
+        mysqli_set_charset($conn,'utf8');
         return $conn;
     }
 
