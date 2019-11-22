c6c520a69e30bee5860c83203035d8247129cce2
Emerson Marcelo <mceloemerson@gmail.com>
2019-11-22 14:59:38 -0300

    Iniciando processo de restrição de acesso

--- src/login/LoginModel.php
+++ src/login/LoginModel.php
@@ -3,17 +3,23 @@
 
     function autenticacao($email, $senha) {
         $conn = get_connection();
-        $sql = "SELECT senha,email FROM usuario WHERE email='$email'";
+        $sql = "SELECT id, senha,email FROM usuario WHERE email='$email'";
         $result = mysqli_query($conn, $sql);
         $erro = "";
         if (mysqli_num_rows($result) > 0) /*serve para validar a autenticação a partir 
         do email e senha */
         {
+            $usuario = [];
             // a condição a seguir serve para fazer a comparação da senha com a hash da mesma
             while($row = mysqli_fetch_assoc($result)) {
                 $hash = $row["senha"];
+                $usuario = $row["nome"];
+                if $row["funcionario"] == 1
+                    $usuario["tipo"] = "funcionario";
+                if $row["cliente"] == 1
+                    $row["tipo"] = "cliente";
                 if (password_verify($senha, $hash)) {
-                    return true;
+                    return $usuario;
                 } else {
                     return false;
                 }
